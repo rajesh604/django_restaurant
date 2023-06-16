265b191394c947873223e2aee58647f3662f8c00
Rajesh <77529419+rajesh604@users.noreply.github.com>
2023-06-16 13:56:42 +0530

    Update settings.py

--- rajeshfoods/rajeshfoods/settings.py
+++ rajeshfoods/rajeshfoods/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq&(o42dp$v0n6$3shmd&nzm-qd()p@=%)pp8zx=c4ziz5y0*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
